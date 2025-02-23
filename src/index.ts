04cf38a615cf874ff5daa5c0e0b592f6f17f80d2
Praveen Randula <167463249+DevPraveenRandula@users.noreply.github.com>
2025-02-23 16:03:57 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(clerkMiddleware({
     secretKey:process.env.CLERK_SECRET_KEY,
 }))
 app.use(cors({
-    origin: "https://fed-storefront-frontend-praveen.netlify.app/",
+    origin: "https://fed-storefront-frontend-praveen.netlify.app",
     credentials: true
 }));
 
